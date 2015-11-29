862099eeda5719e9a724a9bf59211a63ad32c3fc
afnrfcb7 <emory.merryman@gmail.com>
2015-11-29 13:38:10 +0000

    added test

--- test/digitalbird/randomsmoke/forsakenknife/core_test.clj
+++ test/digitalbird/randomsmoke/forsakenknife/core_test.clj
@@ -1,3 +1,8 @@
 (ns digitalbird.randomsmoke.forsakenknife.core-test
   (:require [clojure.test :refer :all]
             [digitalbird.randomsmoke.forsakenknife.core :refer :all]))
+
+(deftest verifyconfigfile-test
+  (testing "verifyconfigfile")
+  (verifyconfigfile (seq (list "" "Host host" "Hostname hostname" "User user" "IdentityFile keyfile" "ControlMaster auto" "ControlPath keyfile_%h_%p_%r")) 0 "host" "hostname" "user" "keyfile")
+)

6e624c5f7813b98d0feabc5381073ed1a4e7f125
afnrfcb7 <emory.merryman@gmail.com>
2015-11-29 13:04:50 +0000

    added verify config method

--- src/digitalbird/randomsmoke/forsakenknife/core.clj
+++ src/digitalbird/randomsmoke/forsakenknife/core.clj
@@ -1,6 +1,18 @@
-(ns digitalbird.randomsmoke.forsakenknife.core)
+(ns digitalbird.randomsmoke.forsakenknife.core
+    [clojure.test :refer :all]
+)
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn verifyconfigfile [config start host hostname user keyfile]
+  (is (.equals "" (get config 1)))
+  (is (.equals (str "Host " host) (get config 2)))
+  (is (.equals (str "Hostname " hostname) (get config 3)))
+  (is (.equals (str "User " user) (get config 4)))
+  (is (.equals (str "IdentityFile " keyfile) (get config 5)))
+  (is (.equals "ControlMaster auto" (get config 6)))
+  (is (.equals (str "ControlPath " keyfile "_%h_%p_%r") (get config 7)))
+)

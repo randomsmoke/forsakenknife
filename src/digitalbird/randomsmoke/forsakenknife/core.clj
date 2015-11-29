ae1b5de08dddc628baeae003adc3a01d734d38a5
afnrfcb7 <emory.merryman@gmail.com>
2015-11-29 13:44:30 +0000

    fixed use nth instead of get

--- src/digitalbird/randomsmoke/forsakenknife/core.clj
+++ src/digitalbird/randomsmoke/forsakenknife/core.clj
@@ -9,10 +9,10 @@
 
 (defn verifyconfigfile [config start host hostname user keyfile]
   (is (.equals "" (get config start)))
-  (is (.equals (str "Host " host) (get config (+ start 1))))
-  (is (.equals (str "Hostname " hostname) (get config (+ start 2))))
-  (is (.equals (str "User " user) (get config (+ start 3))))
-  (is (.equals (str "IdentityFile " keyfile) (get config (+ start 4))))
-  (is (.equals "ControlMaster auto" (get config (+ start 5))))
-  (is (.equals (str "ControlPath " keyfile "_%h_%p_%r") (get config (+ start 6))))
+  (is (.equals (str "Host " host) (nth config (+ start 1))))
+  (is (.equals (str "Hostname " hostname) (nth config (+ start 2))))
+  (is (.equals (str "User " user) (nth config (+ start 3))))
+  (is (.equals (str "IdentityFile " keyfile) (nth config (+ start 4))))
+  (is (.equals "ControlMaster auto" (nth config (+ start 5))))
+  (is (.equals (str "ControlPath " keyfile "_%h_%p_%r") (nth config (+ start 6))))
 )

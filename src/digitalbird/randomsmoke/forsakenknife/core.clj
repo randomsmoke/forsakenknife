3cb75944735011bb618de02b508922781dc3ef03
afnrfcb7 <emory.merryman@gmail.com>
2015-11-29 13:59:09 +0000

    use + instead of inc

--- src/digitalbird/randomsmoke/forsakenknife/core.clj
+++ src/digitalbird/randomsmoke/forsakenknife/core.clj
@@ -9,10 +9,10 @@
 
 (defn verifyconfigfile [config start host hostname user keyfile]
   (is (.equals "" (nth config start)))
-  (is (.equals (str "Host " host) (nth config (inc start 1))))
-  (is (.equals (str "Hostname " hostname) (nth config (inc start 2))))
-  (is (.equals (str "User " user) (nth config (inc start 3))))
-  (is (.equals (str "IdentityFile " keyfile) (nth config (inc start 4))))
-  (is (.equals "ControlMaster auto" (nth config (inc start 5))))
-  (is (.equals (str "ControlPath " keyfile "_%h_%p_%r") (nth config (inc start 6))))
+  (is (.equals (str "Host " host) (nth config (+ start 1))))
+  (is (.equals (str "Hostname " hostname) (nth config (+ start 2))))
+  (is (.equals (str "User " user) (nth config (+ start 3))))
+  (is (.equals (str "IdentityFile " keyfile) (nth config (+ start 4))))
+  (is (.equals "ControlMaster auto" (nth config (+ start 5))))
+  (is (.equals (str "ControlPath " keyfile "_%h_%p_%r") (nth config (+ start 6))))
 )

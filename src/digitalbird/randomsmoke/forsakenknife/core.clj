17fc9b0e8c590391b137a84db39399f6d77dc76f
afnrfcb7 <emory.merryman@gmail.com>
2015-11-29 13:39:39 +0000

    fixed verify to use start index

--- src/digitalbird/randomsmoke/forsakenknife/core.clj
+++ src/digitalbird/randomsmoke/forsakenknife/core.clj
@@ -8,11 +8,11 @@
   (println x "Hello, World!"))
 
 (defn verifyconfigfile [config start host hostname user keyfile]
-  (is (.equals "" (get config 1)))
-  (is (.equals (str "Host " host) (get config 2)))
-  (is (.equals (str "Hostname " hostname) (get config 3)))
-  (is (.equals (str "User " user) (get config 4)))
-  (is (.equals (str "IdentityFile " keyfile) (get config 5)))
-  (is (.equals "ControlMaster auto" (get config 6)))
-  (is (.equals (str "ControlPath " keyfile "_%h_%p_%r") (get config 7)))
+  (is (.equals "" (get config start)))
+  (is (.equals (str "Host " host) (get config (+ start 1)))
+  (is (.equals (str "Hostname " hostname) (get config (+ start 2))))
+  (is (.equals (str "User " user) (get config (+ start 3))))
+  (is (.equals (str "IdentityFile " keyfile) (get config (+ start 4))))
+  (is (.equals "ControlMaster auto" (get config (+ start 5))))
+  (is (.equals (str "ControlPath " keyfile "_%h_%p_%r") (get config (+ start 6))))
 )

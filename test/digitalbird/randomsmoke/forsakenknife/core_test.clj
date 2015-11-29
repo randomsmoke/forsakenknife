e1d2b4367abff6c32fc3f2e5df68117944cad720
afnrfcb7 <emory.merryman@gmail.com>
2015-11-29 13:54:31 +0000

    added unhappy test

--- test/digitalbird/randomsmoke/forsakenknife/core_test.clj
+++ test/digitalbird/randomsmoke/forsakenknife/core_test.clj
@@ -2,7 +2,12 @@
   (:require [clojure.test :refer :all]
             [digitalbird.randomsmoke.forsakenknife.core :refer :all]))
 
-(deftest verifyconfigfile-test
-  (testing "verifyconfigfile")
+(deftest verifyconfigfile-test-happy
+  (testing "This is a happy path test of verifyconfigfile.")
   (verifyconfigfile (seq (list "" "Host host" "Hostname hostname" "User user" "IdentityFile keyfile" "ControlMaster auto" "ControlPath keyfile_%h_%p_%r")) 0 "host" "hostname" "user" "keyfile")
+)
+
+(deftest verifyconfigfile-test-unhappy-start
+  (testing "This is an unhappy path test of verifyconfigfile.  The start index is wrong so everything should not work.")
+  (is (thrown? (verifyconfigfile (seq (list "" "Host host" "Hostname hostname" "User user" "IdentityFile keyfile" "ControlMaster auto" "ControlPath keyfile_%h_%p_%r")) 1 "host" "hostname" "user" "keyfile")))
 )

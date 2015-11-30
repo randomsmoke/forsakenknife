d90ad532b319bc0d536d97fc2c2ae2c449a7ad85
afnrfcb7 <emory.merryman@gmail.com>
2015-11-30 21:25:19 +0000

    adding tests

--- test/digitalbird/randomsmoke/forsakenknife/core_test.clj
+++ test/digitalbird/randomsmoke/forsakenknife/core_test.clj
@@ -1,3 +1,4 @@
 (ns digitalbird.randomsmoke.forsakenknife.core-test
   (:require [clojure.test :refer :all]
             [digitalbird.randomsmoke.forsakenknife.core :refer :all]))
+(deftest verify-hostname-happy-1-a (testing "The hostname in the first stanza is alpha." (is (verify-hostname (seq (list 1 2 "Hostname alpha" 4 5 6 7 8 9 10 11 12 13 14)) 0 "alpha"))))

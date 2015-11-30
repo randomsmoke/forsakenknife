42418651c6dc6d5414d0b6a138ac9ce79cb69065
afnrfcb7 <emory.merryman@gmail.com>
2015-11-30 16:36:52 +0000

    wtf2

--- src/digitalbird/randomsmoke/forsakenknife/core.clj
+++ src/digitalbird/randomsmoke/forsakenknife/core.clj
@@ -6,11 +6,11 @@
   (println x "Hello, World!"))
 
 (defn verifyconfigfile [is config start host hostname user keyfile]
-  (is (.equals "" (nth config start)))
-  (is (.equals (str "Host " host) (nth config (+ start 1))))
-  (is (.equals (str "Hostname " hostname) (nth config (+ start 2))))
+  (is (.equals "" (nth config (* start 7))))
+  (is (.equals (str "Host " host) (nth config (inc (* start 7)))))
+  (is (.equals (str "Hostname " hostname) (nth config (+ (* start 7) 2))))
   (is (.equals (str "User " user) (nth config (+ start 3))))
-  (is (.equals (str "IdentityFile " keyfile) (nth config (+ start 4))))
-  (is (.equals "ControlMaster auto" (nth config (+ start 5))))
-  (is (.equals (str "ControlPath " keyfile "_%h_%p_%r") (nth config (+ start 6))))
+  (is (.equals (str "IdentityFile " keyfile) (nth config (+ (* start 7) 4))))
+  (is (.equals "ControlMaster auto" (nth config (+ (* start 7) 5))))
+  (is (.equals (str "ControlPath " keyfile "_%h_%p_%r") (nth config (+ (* start 7) 6))))
 )

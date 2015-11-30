073a0096e0ca3b1b32a19fc0939e7960f8854a5f
afnrfcb7 <emory.merryman@gmail.com>
2015-11-30 21:22:54 +0000

    moved verification

--- test/digitalbird/randomsmoke/forsakenknife/core_test.clj
+++ test/digitalbird/randomsmoke/forsakenknife/core_test.clj
@@ -1,4 +1,3 @@
 (ns digitalbird.randomsmoke.forsakenknife.core-test
   (:require [clojure.test :refer :all]
             [digitalbird.randomsmoke.forsakenknife.core :refer :all]))
-(defn verify-hostname "Verifies the host name" [config index hostname] (.equals (nth config (+ (* index 7) 2)) (str "Hostname " hostname)))          

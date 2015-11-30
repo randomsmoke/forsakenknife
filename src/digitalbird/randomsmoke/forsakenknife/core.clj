f22781c97a00fdeb7009bcb9135cc73808c63ea9
afnrfcb7 <emory.merryman@gmail.com>
2015-11-30 22:20:28 +0000

    add delims

--- src/digitalbird/randomsmoke/forsakenknife/core.clj
+++ src/digitalbird/randomsmoke/forsakenknife/core.clj
@@ -1,1 +1,9 @@
 (ns digitalbird.randomsmoke.forsakenknife.core)
+; blank
+; host
+; hostname
+; user
+; identity file
+; identities only
+; control master
+; control path
--- test/digitalbird/randomsmoke/forsakenknife/core_test.clj
+++ test/digitalbird/randomsmoke/forsakenknife/core_test.clj
@@ -1,4 +1,11 @@
 (ns digitalbird.randomsmoke.forsakenknife.core-test
   (:require [clojure.test :refer :all]
             [digitalbird.randomsmoke.forsakenknife.core :refer :all]))
-          
+; blank
+; host
+; hostname
+; user
+; identity file
+; identities only
+; control master
+; control path          

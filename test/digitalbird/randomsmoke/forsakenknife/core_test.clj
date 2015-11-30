f81d751c7958e7ab86a0ff5d61714c6a93b8a625
afnrfcb7 <emory.merryman@gmail.com>
2015-11-30 22:09:30 +0000

    adding tests

--- test/digitalbird/randomsmoke/forsakenknife/core_test.clj
+++ test/digitalbird/randomsmoke/forsakenknife/core_test.clj
@@ -5,4 +5,7 @@
 (deftest verify-user-test-happy-1-b (testing "The user in the first stanza is beta.") (is (verify-user (seq (list 1 2 3 "User beta" 5 6 7 8 9 10 11 12 13 14)) 0 "beta")))
 (deftest verify-user-test-happy-2-a (testing "The user in the first stanza is alpha.") (is (verify-user (seq (list 1 2 3 4 5 6 7 8 9 10 "User alpha" 12 13 14)) 1 "alpha")))
 (deftest verify-user-test-happy-2-b (testing "The user in the first stanza is beta.") (is (verify-user (seq (list 1 2 3 4 5 6 7 8 9 10 "User beta" 12 13 14)) 1 "beta")))
-
+(deftest verify-user-test-unhappy-1-a (testing "The user in the first stanza is not alpha.") (is (not (verify-user (seq (list 1 2 3 "User not alpha" 5 6 7 8 9 10 11 12 13 14)) 0 "alpha"))))
+(deftest verify-user-test-unhappy-1-b (testing "The user in the first stanza is not beta.") (is (not (verify-user (seq (list 1 2 3 "User not beta" 5 6 7 8 9 10 11 12 13 14)) 0 "beta"))))
+(deftest verify-user-test-unhappy-2-a (testing "The user in the first stanza is not alpha.") (is (not (verify-user (seq (list 1 2 3 4 5 6 7 8 9 10 "User not alpha" 12 13 14)) 1 "alpha"))))
+(deftest verify-user-test-unhappy-2-b (testing "The user in the first stanza is not beta.") (is (not (verify-user (seq (list 1 2 3 4 5 6 7 8 9 10 "User not beta" 12 13 14)) 1 "beta"))))

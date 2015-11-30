f45422d042fbf299f794f2191f41ad5e62d67473
afnrfcb7 <emory.merryman@gmail.com>
2015-11-30 23:07:27 +0000

    added verify

--- src/digitalbird/randomsmoke/forsakenknife/core.clj
+++ src/digitalbird/randomsmoke/forsakenknife/core.clj
@@ -4,6 +4,7 @@
 ; hostname
 ; user
 ; identity file
+(defn verify-keyfile "Verifies the identity key file" [config index keyfile] (.equals (nth config (+ (* index 7) 3) (str "IdentityFile " keyfile))))
 ; identities only
 ; control master
 ; control path

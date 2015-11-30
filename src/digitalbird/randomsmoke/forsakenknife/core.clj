5ab05d6c1ea8ca8e6330314119bf2ce7837709f0
afnrfcb7 <emory.merryman@gmail.com>
2015-11-30 22:18:32 +0000

    add delims

--- src/digitalbird/randomsmoke/forsakenknife/core.clj
+++ src/digitalbird/randomsmoke/forsakenknife/core.clj
@@ -1,2 +1,3 @@
 (ns digitalbird.randomsmoke.forsakenknife.core)
+; user
 (defn verify-user "Verify the user" [config index user] (.equals (nth config (+ (* index 7) 3)) (str "User " user)))
--- test/digitalbird/randomsmoke/forsakenknife/core_test.clj
+++ test/digitalbird/randomsmoke/forsakenknife/core_test.clj
@@ -1,6 +1,7 @@
 (ns digitalbird.randomsmoke.forsakenknife.core-test
   (:require [clojure.test :refer :all]
             [digitalbird.randomsmoke.forsakenknife.core :refer :all]))
+;user
 (deftest verify-user-test-happy-1-a (testing "The user in the first stanza is alpha.") (is (verify-user (seq (list 1 2 3 "User alpha" 5 6 7 8 9 10 11 12 13 14)) 0 "alpha")))
 (deftest verify-user-test-happy-1-b (testing "The user in the first stanza is beta.") (is (verify-user (seq (list 1 2 3 "User beta" 5 6 7 8 9 10 11 12 13 14)) 0 "beta")))
 (deftest verify-user-test-happy-2-a (testing "The user in the first stanza is alpha.") (is (verify-user (seq (list 1 2 3 4 5 6 7 8 9 10 "User alpha" 12 13 14)) 1 "alpha")))

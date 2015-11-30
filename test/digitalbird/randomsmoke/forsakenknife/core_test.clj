c73b2514ad98862a5151c46a9846d40acc381ad5
afnrfcb7 <emory.merryman@gmail.com>
2015-11-30 21:13:23 +0000

    adding tests

--- test/digitalbird/randomsmoke/forsakenknife/core_test.clj
+++ test/digitalbird/randomsmoke/forsakenknife/core_test.clj
@@ -1,5 +1,5 @@
 (ns digitalbird.randomsmoke.forsakenknife.core-test
   (:require [clojure.test :refer :all]
             [digitalbird.randomsmoke.forsakenknife.core :refer :all]))
-
-(deftest verify-host-happy-1-a (testing "The host in the first stanza is alpha") (is (verify-host (seq (list 1 "alpha" 3 4 5 6 7 8 9 10 11 12 13 14)) 0 "host")))
+(deftest verify-host-happy-1-a (testing "The host in the first stanza is alpha") (is (verify-host (seq (list 1 "Host alpha" 3 4 5 6 7 8 9 10 11 12 13 14)) 0 "alpha")))
+(deftest verify-host-happy-1-b (testing "The host in the first stanza is beta") (is (verify-host (seq (list 1 "Host beta" 3 4 5 6 7 8 9 10 11 12 13 14)) 0 "beta")))

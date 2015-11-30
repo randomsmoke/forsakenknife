089cbac96d2a221e7b605c54af8e9872d701809d
afnrfcb7 <emory.merryman@gmail.com>
2015-11-30 21:35:18 +0000

    add verify

--- test/digitalbird/randomsmoke/forsakenknife/core_test.clj
+++ test/digitalbird/randomsmoke/forsakenknife/core_test.clj
@@ -1,4 +1,4 @@
 (ns digitalbird.randomsmoke.forsakenknife.core-test
   (:require [clojure.test :refer :all]
             [digitalbird.randomsmoke.forsakenknife.core :refer :all]))
-          
+(defn verify-user "Verify the user" [config index user] (.equals (get config (+ (* index 7) 3)) (str "User " user)))

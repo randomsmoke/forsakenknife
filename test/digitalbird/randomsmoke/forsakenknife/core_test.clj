e4dd8a7c657a1ee8eb0c2316a612e32e749681ed
afnrfcb7 <emory.merryman@gmail.com>
2015-11-30 21:51:51 +0000

    adding tests

--- test/digitalbird/randomsmoke/forsakenknife/core_test.clj
+++ test/digitalbird/randomsmoke/forsakenknife/core_test.clj
@@ -1,4 +1,4 @@
 (ns digitalbird.randomsmoke.forsakenknife.core-test
   (:require [clojure.test :refer :all]
             [digitalbird.randomsmoke.forsakenknife.core :refer :all]))
-(deftest verify-user-test-happy-1-a (testing "The user in the first stanza is alpha." (verify-user (seq (list 1 2 3 "User alpha" 5 6 7 8 9 10 11 12 13 14)) 0 "alpha")))
+(deftest verify-user-test-happy-1-a (testing "The user in the first stanza is alpha.") (verify-user (seq (list 1 2 3 "User alpha" 5 6 7 8 9 10 11 12 13 14)) 0 "alpha"))

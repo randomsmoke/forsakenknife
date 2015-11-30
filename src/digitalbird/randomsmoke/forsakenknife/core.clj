cf7293c3615d422bbf28af0e2b69d02773af37b0
afnrfcb7 <emory.merryman@gmail.com>
2015-11-30 17:20:13 +0000

    fixed mistaked in user

--- src/digitalbird/randomsmoke/forsakenknife/core.clj
+++ src/digitalbird/randomsmoke/forsakenknife/core.clj
@@ -9,7 +9,7 @@
   (is (.equals "" (nth config (* start 7))))
   (is (.equals (str "Host " host) (nth config (inc (* start 7)))))
   (is (.equals (str "Hostname " hostname) (nth config (+ (* start 7) 2))))
-  (is (.equals (str "User " user) (nth config (+ start 3))))
+  (is (.equals (str "User " user) (nth config (+ (* start 7) 3))))
   (is (.equals (str "IdentityFile " keyfile) (nth config (+ (* start 7) 4))))
   (is (.equals "ControlMaster auto" (nth config (+ (* start 7) 5))))
   (is (.equals (str "ControlPath " keyfile "_%h_%p_%r") (nth config (+ (* start 7) 6))))

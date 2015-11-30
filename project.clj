1c2f782b4572938e3362a2abe42fcca83dd342e8
afnrfcb7 <emory.merryman@gmail.com>
2015-11-30 14:35:05 +0000

    fixed 1 happy, 1 unhappy test

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject digitalbird.randomsmoke.forsakenknife "0.2.0-SNAPSHOT"
   :description "testing forsakenknife"
-  :repositories [["local" "file://stormyiron"]]
+  :repositories [["local" "file://stormyiron"] ["github" "https://raw.githubusercontent.com/rawflag/stormyiron/master/"]]
   :url "https://github.com/randomsmoke/forsakenknife"
   :license {:name "GNU GPL 3"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"] [digitalbird.easterntombstone/nocturnalhelium "0.4.0-SNAPSHOT"]]
   :plugins [[lein-ancient "0.6.8" :exclusions [org.clojure/clojure]] [lein-kibit "0.1.2"] [jonase/eastwood "0.2.1"] [lein-bikeshed "0.2.0"] [lein-cloverage "1.0.6"]]
   :aliases {"omni" ["do" ["clean"] ["test"] ["with-profile" "production" "deps" ":tree"] ["ancient"] ["kibit"] ["bikeshed" "-m" "1000"] ["cloverage"] ["deploy" "local"]]}
-  )
+)
--- test/digitalbird/randomsmoke/forsakenknife/core_test.clj
+++ test/digitalbird/randomsmoke/forsakenknife/core_test.clj
@@ -1,13 +1,16 @@
 (ns digitalbird.randomsmoke.forsakenknife.core-test
   (:require [clojure.test :refer :all]
-            [digitalbird.randomsmoke.forsakenknife.core :refer :all]))
+            [digitalbird.randomsmoke.forsakenknife.core :refer :all]
+            [digitalbird.easterntombstone.nocturnalhelium.core :refer :all]
+  )
+)
 
 (deftest verifyconfigfile-test-happy
   (testing "This is a happy path test of verifyconfigfile.")
-  (verifyconfigfile (seq (list "" "Host host" "Hostname hostname" "User user" "IdentityFile keyfile" "ControlMaster auto" "ControlPath keyfile_%h_%p_%r")) 0 "host" "hostname" "user" "keyfile")
+  (verifyconfigfile (mock-is RuntimeException) (seq (list "" "Host host" "Hostname hostname" "User user" "IdentityFile keyfile" "ControlMaster auto" "ControlPath keyfile_%h_%p_%r")) 0 "host" "hostname" "user" "keyfile")
 )
 
 (deftest verifyconfigfile-test-unhappy-start
   (testing "This is an unhappy path test of verifyconfigfile.  The start index is wrong so everything should not work.")
-  (is (thrown? (verifyconfigfile (seq (list "" "Host host" "Hostname hostname" "User user" "IdentityFile keyfile" "ControlMaster auto" "ControlPath keyfile_%h_%p_%r")) 1 "host" "hostname" "user" "keyfile")))
+  (is (thrown? RuntimeException (verifyconfigfile (mock-is (new RuntimeException)) (seq (list "" "Host host" "Hostname hostname" "User user" "IdentityFile keyfile" "ControlMaster auto" "ControlPath keyfile_%h_%p_%r")) 1 "host" "hostname" "user" "keyfile")))
 )

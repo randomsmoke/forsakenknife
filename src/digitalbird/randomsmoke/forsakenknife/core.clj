aea8824b3dfeb2f2e83c7443b8abd6c26f2658be
afnrfcb7 <emory.merryman@gmail.com>
2015-11-30 21:28:00 +0000

    adding tests

--- src/digitalbird/randomsmoke/forsakenknife/core.clj
+++ src/digitalbird/randomsmoke/forsakenknife/core.clj
@@ -1,2 +1,2 @@
 (ns digitalbird.randomsmoke.forsakenknife.core)
-(defn verify-hostname "Verifies the host name" [config index hostname] (.equals (nth config (+ (* index 7) 2)) (str "Hostname " hostname)))          
+(defn verify-hostname "Verifies the host name" [config index hostname] (.equals (nth config (+ (* index 7) 2)) (str "Hostname " hostname)))

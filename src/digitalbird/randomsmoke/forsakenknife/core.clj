43eae3fdeb85f8970969b86efc784e5f11cec361
afnrfcb7 <emory.merryman@gmail.com>
2015-11-30 22:35:23 +0000

    added delims

--- src/digitalbird/randomsmoke/forsakenknife/core.clj
+++ src/digitalbird/randomsmoke/forsakenknife/core.clj
@@ -1,2 +1,4 @@
 (ns digitalbird.randomsmoke.forsakenknife.core)
+; hostname
 (defn verify-hostname "Verifies the host name" [config index hostname] (.equals (nth config (+ (* index 7) 2)) (str "Hostname " hostname)))          
+; identity file

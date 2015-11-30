85bf050978231813986ca4a5ded7b64f5cc73f15
afnrfcb7 <emory.merryman@gmail.com>
2015-11-30 20:57:51 +0000

    one test

--- test/digitalbird/randomsmoke/forsakenknife/core_test.clj
+++ test/digitalbird/randomsmoke/forsakenknife/core_test.clj
@@ -1,4 +1,5 @@
 (ns digitalbird.randomsmoke.forsakenknife.core-test
   (:require [clojure.test :refer :all]
             [digitalbird.randomsmoke.forsakenknife.core :refer :all]))
-          
+
+(deftest verify-host-happy-1-a (testing "The host in the first stanza is alpha") (is (verify-host (seq (list 1 "alpha" 3 4 5 6 7 8 9 10 11 12 13 14)) 0 "host")))

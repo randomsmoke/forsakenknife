75e5da769391a64c183c5143f63c3469a6e86ce2
afnrfcb7 <emory.merryman@gmail.com>
2015-12-01 09:43:16 +0000

    fixing test

--- test/digitalbird/randomsmoke/forsakenknife/core_test.clj
+++ test/digitalbird/randomsmoke/forsakenknife/core_test.clj
@@ -7,6 +7,6 @@
 ; user
 ; identity file
 ; identities only
-(deftest verify-identities-only-test-happy-1 (testing "The identities only of the first stanza is correct." (verify-identities-only (seq (list 1 2 3 4 "IdentitiesOnly yes" 6 7 8 9 10 11 12 13 14)) 0)))
+(deftest verify-identities-only-test-happy-1 (testing "The identities only of the first stanza is correct." (verify-identities-only (seq (list 1 2 3 4 5 "IdentitiesOnly yes" 7 8 9 10 11 12 13 14)) 0)))
 ; control master
 ; control path          

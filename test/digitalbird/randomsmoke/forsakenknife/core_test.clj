d711abfd92dce0b36d1c8391f379bcc630f7a97f
afnrfcb7 <emory.merryman@gmail.com>
2015-12-01 11:16:12 +0000

    fixed headers

--- test/digitalbird/randomsmoke/forsakenknife/core_test.clj
+++ test/digitalbird/randomsmoke/forsakenknife/core_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer :all]
             [digitalbird.randomsmoke.forsakenknife.core :refer :all]))
 ; blank
+; host
 (deftest verify-host-happy-1-a (testing "The host in the first stanza is alpha") (is (verify-host (seq (list 1 "Host alpha" 3 4 5 6 7 8 9 10 11 12 13 14)) 0 "alpha")))
 (deftest verify-host-happy-1-b (testing "The host in the first stanza is beta") (is (verify-host (seq (list 1 "Host beta" 3 4 5 6 7 8 9 10 11 12 13 14)) 0 "beta")))
 (deftest verify-host-unhappy-1-a (testing "The host in the first stanza is not alpha") (is (not (verify-host (seq (list 1 "Host not alpha" 3 4 5 6 7 8 9 10 11 12 13 14)) 0 "alpha"))))

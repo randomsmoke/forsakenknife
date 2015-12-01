7588200b516018cf3b7990f43a91d4ef16083171
afnrfcb7 <emory.merryman@gmail.com>
2015-12-01 10:21:26 +0000

    adding test

--- test/digitalbird/randomsmoke/forsakenknife/core_test.clj
+++ test/digitalbird/randomsmoke/forsakenknife/core_test.clj
@@ -8,7 +8,7 @@
 ; identity file
 ; identities only
 (deftest verify-identities-only-test-happy-1 (testing "The identities only of the first stanza is correct." (is (verify-identities-only (seq (list 1 2 3 4 5 "IdentitiesOnly yes" 7 8 9 10 11 12 13 14)) 0))))
-(deftest verify-identities-only-test-happy-2 (testing "The identities only of the second stanza is correct." (is (verify-identities-only (seq (list 1 2 3 4 5 6 7 8 9 10 "IdentitiesOnly yes" 12 13 14)) 1))))
+(deftest verify-identities-only-test-happy-2 (testing "The identities only of the second stanza is correct." (is (verify-identities-only (seq (list 1 2 3 4 5 6 7 8 9 10 11 12 "11 IdentitiesOnly yes" 14)) 1))))
 (deftest verify-identities-only-test-unhappy-1 (testing "The identities only of the first stanza is incorrect." (is (not (verify-identities-only (seq (list 1 2 3 4 5 "IdentitiesOnly not yes" 7 8 9 10 11 12 13 14)) 0)))))
 (deftest verify-identities-only-test-unhappy-2 (testing "The identities only of the second stanza is incorrect." (is (not (verify-identities-only (seq (list 1 2 3 4 5 6 7 8 9 10 11 "IdentitiesOnly not yes" 13 14)) 1)))))
 ; control master

fd8b45108d715b56f00b7642be624c1c7937617f
afnrfcb7 <emory.merryman@gmail.com>
2015-12-01 01:48:43 +0000

    added test

--- test/digitalbird/randomsmoke/forsakenknife/core_test.clj
+++ test/digitalbird/randomsmoke/forsakenknife/core_test.clj
@@ -7,6 +7,6 @@
 ; user
 ; identity file
 ; identities only
-(defn verify-identites-only "Verify identites only" [config index] (.equals (nth config (+ (* config 7) 5))))
+(deftest verify-identities-only-test-happy-1 (testing "The identities only of the first stanza is correct." (verify-identities-only (seq (list 1 2 3 4 5 6 7 8 9 10 11 12 13 14)) 0)))
 ; control master
 ; control path          

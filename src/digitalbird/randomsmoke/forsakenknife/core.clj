33d48443b5296f36facb4fb3d51831df2157551f
afnrfcb7 <emory.merryman@gmail.com>
2015-12-01 09:49:31 +0000

    fixed verify

--- src/digitalbird/randomsmoke/forsakenknife/core.clj
+++ src/digitalbird/randomsmoke/forsakenknife/core.clj
@@ -5,6 +5,6 @@
 ; user
 ; identity file
 ; identities only
-(defn verify-identities-only "Verify identities only" [config index] (.equals (nth config (+ (* index 7) 5))) "IdentitiesOnly yes")
+(defn verify-identities-only "Verify identities only" [config index] (.equals (nth config (+ (* index 7) 5)) "IdentitiesOnly yes"))
 ; control master
 ; control path
